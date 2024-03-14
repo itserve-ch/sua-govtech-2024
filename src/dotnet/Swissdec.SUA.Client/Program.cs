d84e454c3df1a1a80605210f7b104492e7285c48
Sandro Galfo <sandro.galfo@eidias.ch>
2024-03-14 16:13:59 +0100

    Implemented SOAP client with ping service.

---
+++ src/dotnet/Swissdec.SUA.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Swissdec.SUA.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
