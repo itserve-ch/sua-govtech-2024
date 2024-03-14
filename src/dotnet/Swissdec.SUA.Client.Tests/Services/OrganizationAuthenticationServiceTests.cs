375d89daf2a12f3a640bae685de8caec052b5529
Sandro Galfo <sandro.galfo@eidias.ch>
2024-03-14 16:13:59 +0100

    Implemented SOAP client with ping service.

---
+++ src/dotnet/Swissdec.SUA.Client.Tests/Services/OrganizationAuthenticationServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swissdec.SUA.Client.Services.Tests
+{
+    public class OrganizationAuthenticationServiceTests
+    {
+        const string organizationAuthenticationUri = "https://tst.itserve.ch/swissdec/refapps/next/receiver/services/OrganizationAuthenticationService20230301";
+
+        [Fact]
+        public async Task PingAsyncTest()
+        {
+            var pingResponse = await OrganizationAuthenticationService.PingAsync(organizationAuthenticationUri);
+
+            Assert.Equal(1m, pingResponse.UserAgent.StandardVersion);
+        }
+    }
+}
